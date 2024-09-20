b5f51f04360eacef98b54d75d8af7f887a893340
PCassemiro <pedrobr2909@gmail.com>
2024-09-20 16:58:46 -0300

    botão dando erro
    botão de remover não está removendo, e o concluído está sem funções

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function clicar() {
     var tare= window.document.getElementById('tarefa').value;
     var cate=window.document.getElementById('categoria').value;
     var datI=window.document.getElementById('dataInicio').value;
-    var datF=window.document.getElementById('dataFinal').value;
+    var esta='Em andamento';
+    var conc='--/--/----';
     //criando uma variavel li
     var li= document.createElement('li');
     //definindo o dadao da variavel li
@@ -14,13 +15,17 @@ function clicar() {
     Categoria: ${cate}
     </div> 
     <div> 
-    Data de inicio: ${datI}
+    Data de inicio: ${converterData(datI)}
     </div>
-    <div> 
-    Data de término: ${datF}
+    <div id='ClasseEstado'> 
+    Estado: ${esta}
     </div>
     <div>
-    <button style="color:red" onclick=(apagar(this))>apagar</button>
+    Data de conclusão:${conc}
+    </div>
+    <div>
+    <button style="color:green" onclick=(concluir(this))>Concluir</button>
+    <button style="color:red" onclick=(apagar(this))>Apagar</button>
     </div>`;
     //adicionando li na lista
     window.document.getElementById('lista').appendChild(li)
@@ -29,7 +34,24 @@ function clicar() {
 }
 function apagar(botao) {
     event.preventDefault();//pra iguinorar o required(porem tem que ter o this no método)
-    window.Document,alert('Item apagado com sucesso')//só pra testar se a funão ta funcionando
+    alert('Tarefa apagado com sucesso')//só pra testar se a funão ta funcionando
     var li = botao.parentElement;// Encontra o elemento <li> pai do botão
-            li.parentElement.removeChild(li);// Remove o <li> da lista <ul>
+    li.parentElement.removeChild(li);// Remove o <li> da lista <ul>
     }
+
+function converterData(dataISO) {
+    // Separa o ano, mês e dia da string no formato yyyy-MM-dd
+    var partes = dataISO.split('-');
+    var ano = partes[0];
+    var mes = partes[1];
+    var dia = partes[2];
+    // Retorna a data no formato dd/MM/yyyy
+    return `${dia}/${mes}/${ano}`;
+}
+
+function concluir(botao) {
+    alert('Tarefa concluido')
+    // var esatdo=window.document.getElementById('ClasseEstado').value;
+    // var li = botao.parentElement;
+    // esatdo.removeChild('estado')
+}

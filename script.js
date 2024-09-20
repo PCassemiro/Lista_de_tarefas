5f196ae4488a35c51815d952037f14ed610e0b01
PCassemiro <pedrobr2909@gmail.com>
2024-09-20 15:22:21 -0300

    Botão configurado

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 function clicar() {
     //Variavel do input
     var tare= window.document.getElementById('tarefa').value;
+    var cate=window.document.getElementById('categoria').value;
+    var datI=window.document.getElementById('dataInicio').value;
+    var datF=window.document.getElementById('dataFinal').value;
     //criando uma variavel li
     var li= document.createElement('li');
     //definindo o dadao da variavel li
-    // li.textContent= 
-    var count= document.getElementsByClassName('apagado').length;
-
     li.innerHTML = 
-    `<div class="apagado li-${count}">
-    ${tare}<button style="color:red" onclick=(apagar(this))>apagar</button>
-    <script>
-    
-    </script>
-    </div> `;
+    `<div>
+    Tarefa: ${tare}
+    <div> 
+    Categoria: ${cate}
+    </div> 
+    <div> 
+    Data de inicio: ${datI}
+    </div>
+    <div> 
+    Data de término: ${datF}
+    </div>
+    <div>
+    <button style="color:red" onclick=(apagar(this))>apagar</button>
+    </div>`;
     //adicionando li na lista
     window.document.getElementById('lista').appendChild(li)
     //removendo valores que tinham
     window.document.getElementById('tarefa').value = '';
 }
-
 function apagar(botao) {
-    event.preventDefault();
-    
-    // var botao = botao.parentElement;
-    // var teste = botao.className;
-    // teste.innerHTML = '';
-    // teste.value= '';
+    event.preventDefault();//pra iguinorar o required(porem tem que ter o this no método)
+    window.Document,alert('Item apagado com sucesso')//só pra testar se a funão ta funcionando
+    var li = botao.parentElement;// Encontra o elemento <li> pai do botão
+            li.parentElement.removeChild(li);// Remove o <li> da lista <ul>
     }
-
